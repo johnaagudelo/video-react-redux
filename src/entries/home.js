5bb5811f7002a340a81bd68597438b58c47e582b
John Agudelo Galeano <john.agudelo@johnagudelo-2.local>
2018-01-05 17:42:02 -0500

    first commit

---
+++ src/entries/home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { hydrate } from 'react-dom';
+import Home from '../pages/containers/home';
+// import Playlist from './src/playlist/components/playlist';
+import data from '../api.json';
+// console.log('Hola mundo!' )
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const inicialState = {
+    data: { ...data }
+}
+
+const store = createStore(
+    (state) => state,
+    inicialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+console.log(store.getState())
+
+const homeContainer = document.getElementById('home-container')
+
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
+hydrate( 
+    <Provider store={store}>
+        <Home />
+    </Provider>,
+    homeContainer);
+
